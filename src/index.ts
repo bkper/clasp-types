c6887fe842bc21350f6a8a3c20361f3804ecd768
mael <mael@bkper.com>
2019-09-11 08:00:06 -0300

    Reading data from .clasp.json

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,14 @@ import * as fs from "fs-extra";
 import { Builder } from "./Builder";
 
 let rawdata = fs.readFileSync('data/library.json');
+let claspdata = fs.readFileSync('.clasp.json');
+
 let rootNode = JSON.parse(rawdata.toString());
+let clasp = JSON.parse(claspdata.toString());
 
-let builder = new Builder(rootNode, "Bkper", "BkperApp");
+let libraryNamespace: string = clasp.library.namespace;
+let libraryName: string = clasp.library.name;
 
-fs.outputFileSync('build/google-apps-script.bkper/index.d.ts', builder.buildLibrary());
+let builder = new Builder(rootNode, libraryNamespace, libraryName);
+
+fs.outputFileSync(`build/google-apps-script.${libraryNamespace.toLowerCase()}/index.d.ts`, builder.buildLibrary());
