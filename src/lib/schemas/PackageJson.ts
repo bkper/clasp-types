d411a7f5fd95986a38547e649f2f8eec353e4f07
mael <mael@bkper.com>
2019-09-13 13:15:16 -0300

    Adding props to be cleaned up

--- src/lib/schemas/PackageJson.ts
+++ src/lib/schemas/PackageJson.ts
@@ -1,8 +1,12 @@
 export interface PackageJson {
+  name: string,
   homepage: string,
   repository: string | { type: string, url: string }
   author: string | PackageJsonAuthor | null,
   contributors: PackageJsonAuthor[]
+  scripts: any
+  devDependencies: any
+  types: string
 }
 
 interface PackageJsonAuthor {
